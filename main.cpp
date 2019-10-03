aaa1f53897b35a900377182f9c3833c3520f4945
ethanharrison <harrem18@wfu.edu>
2019-10-03 14:43:12 -0400

    Implemented setters

--- main.cpp
+++ main.cpp
@@ -47,6 +47,27 @@ void Faculty::setSalary(double s) {
 
 /* 2) implement each setter and getter */
 
+void Faculty::setName(string n) {
+    name = n;
+}
+
+void Faculty::setID(string i) {
+    id = i;
+}
+
+void Faculty::setAddress(string a){
+    address = a;
+}
+
+void Faculty::setDoB(string d) {
+    DoB = d;
+}
+
+void Faculty::setDegree(string e) {
+    degree = e;
+}
+
+
 int main() {
     Faculty prof;
 
