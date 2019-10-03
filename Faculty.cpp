623f6bb5f64679d03844ec98597ad34f920e2c41
ethanharrison <harrem18@wfu.edu>
2019-10-03 15:07:18 -0400

    Move classes/functions to new files

--- Faculty.cpp
+++ Faculty.cpp
@@ -2,3 +2,30 @@
 // Created by Ethan Harrison on 10/3/19.
 //
 
+#include "Faculty.h"
+
+void Faculty::setSalary(double s) {
+    salary = s;
+}
+
+/* 2) implement each setter and getter */
+
+void Faculty::setName(string n) {
+    name = n;
+}
+
+void Faculty::setID(string i) {
+    id = i;
+}
+
+void Faculty::setAddress(string a) {
+    address = a;
+}
+
+void Faculty::setDoB(string d) {
+    DoB = d;
+}
+
+void Faculty::setDegree(string e) {
+    degree = e;
+}
--- Faculty.h
+++ Faculty.h
@@ -5,4 +5,60 @@
 #ifndef CLASSEXCERCISE_FACULTY_H
 #define CLASSEXCERCISE_FACULTY_H
 
+class Faculty {
+public:
+    Faculty();//default constructor
+    string name;
+    string id;
+    string address;
+    string DoB;
+    string degree;
+
+//getter and setter for salary
+    double getSalary() { return salary; }
+
+    void setSalary(double salary);
+
+    /* 1) create a getter and setter for each variable in the class*/
+
+    string getName() { return name; }
+
+    void setName(string name);
+
+    string getID() { return id; }
+
+    void setID(string id);
+
+    string getAddress() { return address; }
+
+    void setAddress(string address);
+
+    string getDoB() { return DoB; }
+
+    void setDoB(string DoB);
+
+    string getDegree() { return degree; }
+
+    void setDegree(string degree);
+
+
+    //getter and setter for SSN
+    double getSSN() { return SSN; }
+
+    void setSSN(double SSN);
+
+private:
+    double salary;
+    double SSN;
+};
+
+//General Constructor
+Faculty::Faculty(string nm, string ident, string add, string date, string deg) {
+    name = nm;
+    id = ident;
+    address = add;
+    DoB = date;
+    degree = deg;
+}
+
 #endif //CLASSEXCERCISE_FACULTY_H
